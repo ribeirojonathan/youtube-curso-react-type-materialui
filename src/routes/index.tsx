0b2135ca488a892f00a225199c66d94d01fefb74
jonathan.souza <jonathan.souza@e-unicred.com.br>
2026-01-20 14:38:05 -0300

    instalação material ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export { };
+import { Button } from "@mui/material";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+
+    return(
+        <Routes>
+            <Route path="/pagina-inicial" element={<Button> teste </Button>} />
+
+            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+        </Routes>
+    );
+}
